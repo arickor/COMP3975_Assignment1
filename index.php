b1c71a5104e48f095f7b3bb69b67a0c3ee5a4c79
arickor <aricor10945@gmail.com>
2024-02-20 22:50:34 -0800

    Checked whether Transactions table is empty before inserting sample data

--- index.php
+++ index.php
@@ -28,20 +28,26 @@ $SQL_create_table .= "CREATE TABLE IF NOT EXISTS Users (
 
 $db->exec($SQL_create_table);
 
-$file = fopen('2023 02.csv', 'r');
+$result = $db->query("SELECT COUNT(*) as count FROM Transactions");
+$row = $result->fetchArray();
+$count = $row['count'];
 
-while (($line = fgetcsv($file)) !== FALSE) {
-    $date = SQLite3::escapeString($line[0]);
-    $shopName = SQLite3::escapeString($line[1]);
-    $moneySpent = SQLite3::escapeString($line[2]);
-    $moneyDeposited = SQLite3::escapeString($line[3]);
-    $bankBalance = SQLite3::escapeString($line[4]);
+if ($count == 0) {
+    $file = fopen('2023 02.csv', 'r');
 
-    $SQL_insert = "INSERT INTO Transactions (Date, ShopName, MoneySpent, MoneyDeposited, BankBalance) 
-                 VALUES ('$date', '$shopName', '$moneySpent', '$moneyDeposited', '$bankBalance');";
-    $db->exec($SQL_insert);
+    while (($line = fgetcsv($file)) !== FALSE) {
+        $date = SQLite3::escapeString($line[0]);
+        $shopName = SQLite3::escapeString($line[1]);
+        $moneySpent = SQLite3::escapeString($line[2]);
+        $moneyDeposited = SQLite3::escapeString($line[3]);
+        $bankBalance = SQLite3::escapeString($line[4]);
+
+        $SQL_insert = "INSERT INTO Transactions (Date, ShopName, MoneySpent, MoneyDeposited, BankBalance) 
+                     VALUES ('$date', '$shopName', '$moneySpent', '$moneyDeposited', '$bankBalance');";
+        $db->exec($SQL_insert);
+    }
+
+    fclose($file);
 }
-
-fclose($file);
 
 $db->close();
