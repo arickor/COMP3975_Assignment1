b5c27632c752f92823249b3835c7c63a5f552233
arickor <aricor10945@gmail.com>
2024-02-20 23:16:01 -0800

    Organized code structure

---
+++ include_db.php
@@ -0,0 +1,5 @@
+<?php
+
+$db = new SQLite3(__DIR__ . '/expenses.sqlite');
+
+?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new SQLite3('expenses.sqlite');
+include 'include_db.php';
 
 $SQL_create_table = "CREATE TABLE IF NOT EXISTS Transactions (
     Date DATE NOT NULL,
@@ -27,27 +27,5 @@ $SQL_create_table .= "CREATE TABLE IF NOT EXISTS Users (
 );";
 
 $db->exec($SQL_create_table);
-
-$result = $db->query("SELECT COUNT(*) as count FROM Transactions");
-$row = $result->fetchArray();
-$count = $row['count'];
-
-if ($count == 0) {
-    $file = fopen('2023 02.csv', 'r');
-
-    while (($line = fgetcsv($file)) !== FALSE) {
-        $date = SQLite3::escapeString($line[0]);
-        $shopName = SQLite3::escapeString($line[1]);
-        $moneySpent = SQLite3::escapeString($line[2]);
-        $moneyDeposited = SQLite3::escapeString($line[3]);
-        $bankBalance = SQLite3::escapeString($line[4]);
-
-        $SQL_insert = "INSERT INTO Transactions (Date, ShopName, MoneySpent, MoneyDeposited, BankBalance) 
-                     VALUES ('$date', '$shopName', '$moneySpent', '$moneyDeposited', '$bankBalance');";
-        $db->exec($SQL_insert);
-    }
-
-    fclose($file);
-}
 
 $db->close();
