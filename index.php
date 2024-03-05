a0e06b872cb2c98550122b5332a5c4d07ddc9686
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-03-04 18:01:22 -0800

    Fix the error on login page

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
 session_start();
 
-// Check if the user is logged in, if not then redirect to login page
-if ($_SESSION["loggedin"]) {
+if (isset($_SESSION["loggedin"])) {
     header("location: /login/index.php");
-    exit;
 }
-if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+// Check if the user is logged in, if not then redirect to login page
+if ($_SESSION["loggedin"] !== true) {
     header("location: /login/index.php");
     $_SESSION['authorization_error'] = 'Please login to view the page';
     exit;
