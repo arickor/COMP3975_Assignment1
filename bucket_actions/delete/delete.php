867207cc435b0449eaeacbac11d6a9b42ad43605
Jeongeun Oh <ohjeoung5224@gmail.com>
2024-03-05 21:32:27 -0800

    Add bootstrap for delete bucket page

--- bucket_actions/delete/delete.php
+++ bucket_actions/delete/delete.php
@@ -1,5 +1,3 @@
-<h1>Delete Bucket</h1>
-
 <?php
 
 include $_SERVER['DOCUMENT_ROOT'] . '/include_db.php';
@@ -35,26 +33,53 @@ if (isset($_GET['shopName'])) {
 
 $db->close();
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<table>
-    <tr>
-        <td>Shop Name: </td>
-        <td><?php echo $shopName ?></td>
-    </tr>
-    <tr>
-        <td>Category: </td>
-        <td><?php echo $category ?></td>
-    </tr>
-</table>
-<br />
-<form action="process_delete.php" method="post">
-    <input type="hidden" value="<?php echo $shopName ?>" name="shopName" />
-    <a href="../../index.php" class="btn btn-small btn-primary">&lt;&lt; BACK</a>
-    &nbsp;&nbsp;&nbsp;
-    <input type="submit" value="Delete" class="btn btn-danger" name="delete" />
-</form>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Delete Bucket</title>
 
-<br />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+
+    <!-- Include jQuery library -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h1>Delete Bucket</h1>
+
+        <div class="row">
+            <div class="col-md-4">
+                <table class="table">
+                    <tr>
+                        <td>Shop Name </td>
+                        <td><?php echo $shopName ?></td>
+                    </tr>
+                    <tr>
+                        <td>Category: </td>
+                        <td><?php echo $category ?></td>
+                    </tr>
+                </table>
+                <br />
+                <form action="process_delete.php" method="post">
+                    <input type="hidden" value="<?php echo $shopName ?>" name="shopName" />
+                    <a href="../../index.php" class="btn btn-primary btn-sm">&lt;&lt; BACK</a>
+                    &nbsp;&nbsp;&nbsp;
+                    <input type="submit" value="Delete" class="btn btn-danger" name="delete" />
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- JS, Popper.js, and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
 <?php
 include '../../footer.php';
 ?>
+
+</html>
