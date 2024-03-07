f2e98b4d1ca3cf08b261672825cb9c4f635ba443
arickor <aricor10945@gmail.com>
2024-03-06 19:01:22 -0800

    Removed import index.php

--- import/index.php
+++ import/index.php
@@ -1,27 +1,29 @@
 <?php
 
-include $_SERVER['DOCUMENT_ROOT'] . '/include_db.php';
+// include $_SERVER['DOCUMENT_ROOT'] . '/include_db.php';
 
-$result = $db->query("SELECT COUNT(*) as count FROM Transactions");
-$row = $result->fetchArray();
-$count = $row['count'];
+// $result = $db->query("SELECT COUNT(*) as count FROM Transactions");
+// $row = $result->fetchArray();
+// $count = $row['count'];
 
-if ($count == 0) {
-    $file = fopen('data/2023 02.csv', 'r');
+// if ($count == 0) {
+//     $file = fopen('data/2023 02.csv', 'r');
 
-    while (($line = fgetcsv($file)) !== FALSE) {
-        $date = SQLite3::escapeString($line[0]);
-        $shopName = SQLite3::escapeString($line[1]);
-        $moneySpent = SQLite3::escapeString($line[2]);
-        $moneyDeposited = SQLite3::escapeString($line[3]);
-        $bankBalance = SQLite3::escapeString($line[4]);
+//     while (($line = fgetcsv($file)) !== FALSE) {
+//         $date = SQLite3::escapeString($line[0]);
+//         $shopName = SQLite3::escapeString($line[1]);
+//         $moneySpent = SQLite3::escapeString($line[2]);
+//         $moneyDeposited = SQLite3::escapeString($line[3]);
+//         $bankBalance = SQLite3::escapeString($line[4]);
 
-        $SQL_insert = "INSERT INTO Transactions (Date, ShopName, MoneySpent, MoneyDeposited, BankBalance) 
-                     VALUES ('$date', '$shopName', '$moneySpent', '$moneyDeposited', '$bankBalance');";
-        $db->exec($SQL_insert);
-    }
+//         $SQL_insert = "INSERT INTO Transactions (Date, ShopName, MoneySpent, MoneyDeposited, BankBalance) 
+//                      VALUES ('$date', '$shopName', '$moneySpent', '$moneyDeposited', '$bankBalance');";
+//         $db->exec($SQL_insert);
+//     }
 
-    fclose($file);
-}
+//     fclose($file);
+// }
 
-$db->close();
+// $db->close();
+
+?>
